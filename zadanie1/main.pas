41fbb87e8572b1fdc189102638d536c53d983ab1
epnaj <jan.pieniazek@student.uj.edu.pl>
2026-03-30 20:19:20 +0200

    Zadanie 1 - 4.0

--- zadanie1/main.pas
+++ zadanie1/main.pas
@@ -1,26 +1,27 @@
 program BS;
 
-const size = 50;
-const range = 100;
+{ const size = 50; }
+{ const range = 100; }
 
-type MyArray = array[1..size] of integer;
+type MyArray = array of integer;
 
 var liczby: MyArray;
 
-procedure Generate(var arr: MyArray);
+procedure Generate(var arr: MyArray; range_l, range_r, n: integer);
 var i: integer;
 
 begin
+  SetLength(arr, n);
   Randomize;
-  for i := 1 to size do
-    arr[i] := Random(range + 1);
+  for i := 0 to n - 1 do
+    arr[i] := Random(range_r - range_l + 1) + range_l;
 end;
 
 procedure PrintArray(arr: MyArray);
 var i: integer;
 
 begin
-  for i := 1 to size do
+  for i := 0 to Length(arr) - 1 do
     write(arr[i], ' ');
   writeln;
 end;
@@ -29,8 +30,8 @@ procedure BubbleSort(var arr: MyArray);
 var i, j, temp: integer;
 
 begin
-  for i := 1 to size - 1 do
-    for j := 1 to size - i do
+  for i := 0 to Length(arr) - 2 do
+    for j := 0 to Length(arr) - i - 2 do
       if arr[j] > arr[j + 1] then
       begin
         temp := arr[j];
@@ -40,7 +41,7 @@ begin
 end;
 
 begin
-  Generate(liczby);
+  Generate(liczby, 3, 10, 21);
   writeln('Numbers:');
   PrintArray(liczby);
 
