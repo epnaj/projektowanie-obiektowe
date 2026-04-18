d7ddbe4985d9d9bec8832e3b3a59d5978ccd9cd7
epnaj <jan.pieniazek@student.uj.edu.pl>
2026-04-18 23:28:36 +0200

    Zadanie 4 - 4.0

---
+++ zadanie3/src/main/kotlin/com/example/zadanie3/AuthController.kt
@@ -0,0 +1,21 @@
+package com.example.zadanie3
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+data class LoginRequest(val username: String, val password: String)
+
+data class LoginResponse(val authenticated: Boolean)
+
+@RestController
+@RequestMapping("/login")
+class AuthController {
+
+    @PostMapping
+    fun login(@RequestBody request: LoginRequest): LoginResponse {
+        val authenticated = AuthService.authenticate(request.username, request.password)
+        return LoginResponse(authenticated)
+    }
+}
